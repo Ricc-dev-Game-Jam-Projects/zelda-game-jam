e98c35c1ef782e24b045f96d1063463e1e3fa554
Lucas Souza <42611011+LucasSNSantos@users.noreply.github.com>
2019-09-25 21:05:46 -0300

    initial Commit

---
+++ Assets/Scripts/Drops/Dropable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Drops
+{
+    abstract class Dropable
+    {
+        protected float DropRating { get; set; }
+        protected string SpritePath { get; set; }
+
+        public abstract float IncreaseDropRating();
+        public abstract float DecreaseDropRating(int parameter);
+    }
+}
---
+++ Assets/Scripts/Drops/Hearts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Drops
+{
+    class Hearts : Dropable
+    {
+        public override float DecreaseDropRating(int parameter)
+        {
+            this.DropRating = this.DropRating - parameter;
+            return this.DropRating;
+        }
+
+        public override float IncreaseDropRating()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
