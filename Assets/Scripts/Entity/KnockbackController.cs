7816095abff68844e85e400b08739acb959c02d8
JoelQ <joelquey@hotmail.com>
2019-09-27 23:45:04 +0800

    - Updated knockbackController.

--- Assets/Scripts/Entity/KnockbackController.cs
+++ Assets/Scripts/Entity/KnockbackController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
+using Pathfinding;
 
 public class KnockbackController : MonoBehaviour
 {
@@ -7,17 +8,20 @@ public class KnockbackController : MonoBehaviour
     [SerializeField] private float duration = 0.2f;
     private Enemy enemy;
     private Rigidbody2D rb;
+    private AIPath path;
 
     private void Start()
     {
         enemy = GetComponent<Enemy>();
         enemy.OnTakeDamageEvent += ApplyKnockback;
         rb = enemy.MyRoot.GetComponent<Rigidbody2D>();
+        path = enemy.MyRoot.GetComponent<AIPath>();
     }
 
     private void ApplyKnockback()
     {
         Debug.Log($"Applied knockback {enemy.MyRoot.transform.up * -force}");
+        path.canMove = false;
         rb.AddForce(enemy.MyRoot.transform.up * -force, ForceMode2D.Impulse);
         StartCoroutine(RemoveKnockback());
     }
@@ -26,5 +30,6 @@ public class KnockbackController : MonoBehaviour
     {
         yield return new WaitForSeconds(duration);
         rb.velocity = Vector2.zero;
+        path.canMove = true;
     }
 }
